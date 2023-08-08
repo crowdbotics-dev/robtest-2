65804aea6ed14c3ee4517da650fba562ef7bdc3d
Robert So <robert.so@crowdbotics.com>
2023-08-08 08:19:47 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "robtest-2",
-  url: "https://robtest-2.botics.co",
-  api: "https://robtest-2.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "robtest-2",
+  "url": "https://robtest-2.botics.co",
+  "api": "https://robtest-2.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "Untitled2"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
